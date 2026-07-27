85c66d30e4ed4dc0ee568f75258365238d371891
githubrana2023 <121456055+githubrana2023@users.noreply.github.com>
2026-07-27 11:36:50 +0000

    own tab component building

--- components/shared/tab.tsx
+++ components/shared/tab.tsx
@@ -1,14 +1,44 @@
 import { View, Text } from 'react-native'
-import React, { JSX } from 'react'
+import React, { ComponentType, JSX, useState } from 'react'
+import Container from './container';
+import { Label } from '../ui/label';
+import { cn } from '@/lib/utils';
 
-const Tab = ({ tabName, tabContent }: {
-    tabContent: Record<string, () => JSX.Element>;
-    tabName: readonly string[]
+
+const Tab = <T extends readonly string[],>({ tabLabels, tabContent }: {
+    tabContent: Record<T[number], ComponentType>;
+    tabLabels: T
 }) => {
+    const [activeTab, setActivTab] = useState<typeof tabLabels[number]>(tabLabels[0])
+
+    const TabContent = tabContent[activeTab]
+
     return (
-        <View>
-            <Text>Tab</Text>
-        </View>
+        <Container>
+            <View className="flex-1">
+                <View className='flex-1'>
+                    <TabContent />
+                </View>
+
+                {/* Toggle button */}
+                <View className="flex-row items-center justify-center ">
+                    <View className="flex-row items-center justify-center gap-2 p-2 bg-secondary rounded-full">
+                        {
+                            tabLabels.map(tabLabel => {
+                                const actived = tabLabel === activeTab
+                                return (
+                                    <Label
+                                        className={cn("px-2 py-0.5 rounded-full", actived && 'bg-white')}
+                                        onPress={() => setActivTab(tabLabel)}>
+                                        Damage & Discount
+                                    </Label>
+                                )
+                            })
+                        }
+                    </View>
+                </View>
+            </View>
+        </Container>
     )
 }
 
